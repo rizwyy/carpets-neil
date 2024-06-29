e9670c554e72727515f86f4f3c52b3ccd6559692
Riswin Mohammed <123391105+rizwyy@users.noreply.github.com>
2024-06-29 10:56:31 +0530

    update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: false },
-  modules: ["@nuxtjs/tailwindcss", "@nuxt/fonts", "@pinia/nuxt", "@nuxtjs/supabase"],
+  modules: [
+    "@nuxtjs/tailwindcss",
+    "@nuxt/fonts",
+    "@pinia/nuxt",
+    "@nuxtjs/supabase",
+  ],
   app: {
     pageTransition: { name: "page", mode: "out-in" },
   },
