38f1464202a5ff6e4ba39a76c90aba9540392d01
Riswin Mohammed <123391105+rizwyy@users.noreply.github.com>
2024-07-04 08:55:50 +0530

    s

--- components/MOBILE/FLOORING/MaterialDetails.vue
+++ components/MOBILE/FLOORING/MaterialDetails.vue
@@ -306,7 +306,7 @@
           placeholder
           loading="lazy"
           class="h-full w-full object-cover absolute left-0 bottom-0 z-[-9]"
-          src="/carpets-tile.jpeg"
+          src="/carpets/sisal.jpeg"
           alt="#"
         />
         <span>Sisal</span>
