4fb3c64db176073a7b97f07864d66e28c885e953
Riswin Mohammed <123391105+rizwyy@users.noreply.github.com>
2024-07-04 22:12:30 +0530

    sd

--- components/MOBILE/FLOORING/CatalogItemMOB.vue
+++ components/MOBILE/FLOORING/CatalogItemMOB.vue
@@ -14,6 +14,8 @@ const route = useRoute();
   <div class="h-max w-[46vw] flex flex-col items-center">
     <div class="NuxtImgContainer h-[24vh] w-full overflow-hidden bg-gray-200">
       <NuxtImg
+        placeholder
+        loading="eager"
         class="object-cover h-full w-full"
         :src="`/carpets/${color}.jpg`"
         :alt="color"
