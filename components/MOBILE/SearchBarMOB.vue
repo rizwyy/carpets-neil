8ddd54122a8600d25ab45a7195451190b905bfd7
Riswin Mohammed <123391105+rizwyy@users.noreply.github.com>
2024-07-05 16:34:08 +0530

    search-bar-updated

--- components/MOBILE/SearchBarMOB.vue
+++ components/MOBILE/SearchBarMOB.vue
@@ -1,8 +1,10 @@
 <template>
   <div
-    class="relative h-max w-screen flex flex-col items-center justify-center mt-[2vh]"
+    class="relative h-max w-screen flex flex-col items-center justify-center my-[1.8vh] px-[8vw]"
   >
-    <div class="div_searchBar h-max w-full flex items-center pl-[8vw] pr-[8vw]">
+    <div
+      class="div_searchBar h-max w-full flex items-center bg-[#f1f1f1] px-[1vw] rounded-md"
+    >
       <input
         v-model="query"
         @input="onInput"
@@ -10,20 +12,20 @@
         @blur="onBlur"
         type="text"
         placeholder="Search..."
-        class="input_searchBar w-full flex-grow py-[1vh] border-b-[1px] border-black rounded-sm pl-[4vw] outline-none focus:outline-none"
+        class="input_searchBar h-max w-full bg-[#f1f1f1] flex-grow py-[1.8vh] rounded-sm pl-[4vw] outline-none focus:outline-none"
       />
       <button
         @click="onSearch"
-        class="flex items-center justify-center bg-white text-black pr-[2vw]"
+        class="flex h-full items-center justify-center px-[2vw] py-[1.2vh] rounded-full bg-slate-800"
       >
         <svg
           xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
+          width="20"
+          height="20"
           viewBox="0 0 24 24"
         >
           <path
-            fill="currentColor"
+            fill="#f1f1f1"
             d="m19.6 21l-6.3-6.3q-.75.6-1.725.95T9.5 16q-2.725 0-4.612-1.888T3 9.5t1.888-4.612T9.5 3t4.613 1.888T16 9.5q0 1.1-.35 2.075T14.7 13.3l6.3 6.3zM9.5 14q1.875 0 3.188-1.312T14 9.5t-1.312-3.187T9.5 5T6.313 6.313T5 9.5t1.313 3.188T9.5 14"
           />
         </svg>
