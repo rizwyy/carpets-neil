d979fac75dba346ef13ed3263c84f2b2c75aafb4
Riswin Mohammed <123391105+rizwyy@users.noreply.github.com>
2024-07-06 11:01:51 +0530

    done

--- components/MOBILE/CustomCarpetBTN.vue
+++ components/MOBILE/CustomCarpetBTN.vue
@@ -1,11 +1,12 @@
 <template>
   <Desktop>
-    <button
+    <div
       class="h-[16vh] w-[60vw] relative overflow-hidden flex items-center gap-[2vw] justify-center w-[52vw] text-[#fff] shadow-xl rounded-md bg-gradient-to-br from-[#0e0725] via-[#222d52] to-[#e536ab] px-[.8vh] py-[.8vh]"
     >
       <span class="text-[3.4vh] font font-[600]">Custom Carpets</span>
 
-      <span
+      <a
+        href="/flooring/carpets/custom-carpets"
         class="absolute bottom-[1vh] right-[4vw] text-[#ececec] text-[2vh] font-[500] flex items-center gap-[.6vw]"
         >View All<svg
           xmlns="http://www.w3.org/2000/svg"
@@ -18,8 +19,8 @@
             d="M16.15 13H5q-.425 0-.712-.288T4 12t.288-.712T5 11h11.15L13.3 8.15q-.3-.3-.288-.7t.288-.7q.3-.3.713-.312t.712.287L19.3 11.3q.15.15.213.325t.062.375t-.062.375t-.213.325l-4.575 4.575q-.3.3-.712.288t-.713-.313q-.275-.3-.288-.7t.288-.7z"
           />
         </svg>
-      </span>
-    </button>
+      </a>
+    </div>
   </Desktop>
   <section class="h-max w-max min-[990px]:hidden">
     <button
@@ -27,7 +28,8 @@
     >
       <span class="text-[2.8vh] font font-[600]">Custom Carpets</span>
 
-      <span
+      <a
+        href="/flooring/carpets/custom-carpets"
         class="absolute bottom-[1vh] right-[4vw] text-[#ececec] text-[1.6vh] font-[500] flex items-center gap-[.6vw]"
         >View All<svg
           xmlns="http://www.w3.org/2000/svg"
@@ -40,7 +42,7 @@
             d="M16.15 13H5q-.425 0-.712-.288T4 12t.288-.712T5 11h11.15L13.3 8.15q-.3-.3-.288-.7t.288-.7q.3-.3.713-.312t.712.287L19.3 11.3q.15.15.213.325t.062.375t-.062.375t-.213.325l-4.575 4.575q-.3.3-.712.288t-.713-.313q-.275-.3-.288-.7t.288-.7z"
           />
         </svg>
-      </span>
+      </a>
     </button>
   </section>
 </template>
--- components/MOBILE/UnderDev.vue
+++ components/MOBILE/UnderDev.vue
@@ -1,7 +1,24 @@
 <template>
-  <div class="h-screen w-screen overflow-hidden bg-gradient-to-br from"></div>
+  <div
+    class="h-[88vh] w-screen overflow-hidden bg-gradient-to-b from-[#fff] via-[#f1f1f1] to-[#ececec] from-[1%] flex items-center justify-center"
+  >
+    <div
+      class="flex h-max w-max items-center flex flex-col justify-center gap-[2vh] px-[2vw] text-center"
+    >
+      <span class="text-[#222] text-[4.2vh] font-outfit">
+        This page is under development. Please check back soon.
+      </span>
+      <NuxtImg class="h-[18vh]" src="/workInProgress.png" />
+    </div>
+  </div>
 </template>
 
 <script setup></script>
 
-<style lang="scss" scoped></style>
+<style scoped>
+.font-outfit {
+  font-family: "Outfit", sans-serif;
+  font-optical-sizing: auto;
+  font-style: normal;
+}
+</style>
