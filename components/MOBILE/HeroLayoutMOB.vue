bfa093276eca5b3bbc5def6b9afeb9f7de8c3096
Riswin Mohammed <123391105+rizwyy@users.noreply.github.com>
2024-07-07 20:48:04 +0530

    done

--- components/MOBILE/HeroLayoutMOB.vue
+++ components/MOBILE/HeroLayoutMOB.vue
@@ -42,7 +42,7 @@
             COMING SOON!
           </div>
           <span class="text-[3vh] font-[400] leading-[3.2vh] z-[1] text-[#fff9]"
-            >Furniture</span
+            >Wall Covering</span
           >
           <div class="h-max w-full px-[2vw] flex justify-end">
             <!-- <a
@@ -58,131 +58,7 @@
             quality="20"
             loading="lazy"
             class="absolute object-cover bottom-0 left-0 h-full w-full z-[-99]"
-            src="/60001.avif"
-            alt="#"
-          />
-        </div>
-      </div>
-      <div class="h-max w-screen flex justify-evenly gap-[1vw] px-[2vw]">
-        <div
-          class="h-[24vh] z-[1] w-[50%] overflow-hidden relative flex flex-col gap-[.8vh] items-start justify-between px-[1vw] pt-[1.8vh] pb-[1.2vh]"
-        >
-          <div
-            class="h-max w-full absolute top-[50%] left-0 text-center z-[9] bg-[#888] bg-opacity-[.4] backdrop-blur-[8px] text-[#fcfcfc] text-[1.8vh] py-[.8vh]"
-          >
-            COMING SOON!
-          </div>
-          <span
-            class="text-[3vh] font-[400] w-[50%] leading-[3.2vh] z-[1] text-[#fff9]"
-            >Blinds & Curtains</span
-          >
-          <div class="h-max w-full px-[2vw] flex justify-end">
-            <!-- <a
-              class="underline underline-offset-[.4vh] z-[99] text-[#e1e9e5] text-[1.8vh] font-[500] ml-[.2vw] z-[1]"
-              href="/carpets"
-              >view all</a
-            > -->
-          </div>
-          <div
-            class="absolute object-cover bottom-0 left-0 h-full w-full z-[-9] opacity-[.7] bg-black"
-          ></div>
-          <NuxtImg
-            quality="20"
-            loading="lazy"
-            class="absolute object-cover bottom-0 left-0 h-full w-full z-[-99]"
-            src="/10004.jpg"
-            alt="#"
-          />
-        </div>
-        <div
-          class="h-[24vh] z-[1] w-[50%] overflow-hidden relative flex flex-col gap-[.8vh] items-start justify-between px-[1vw] pt-[1.8vh] pb-[1.2vh]"
-        >
-          <div
-            class="h-max w-full absolute top-[50%] left-0 text-center z-[9] bg-[#888] bg-opacity-[.4] backdrop-blur-[8px] text-[#fcfcfc] text-[1.8vh] py-[.8vh]"
-          >
-            COMING SOON!
-          </div>
-          <span
-            class="text-[3vh] w-[75%] font-[400] leading-[3.2vh] text-[#fff9]"
-            >3D Printed <br />Decor</span
-          >
-          <div class="h-max w-full px-[2vw] flex justify-end">
-            <!-- <a
-              class="underline underline-offset-[.4vh] z-[99] text-[#e1e9e5] text-[1.8vh] font-[500] ml-[.2vw]"
-              href="/raised-floors"
-              >view all</a
-            > -->
-          </div>
-          <div
-            class="absolute object-cover bottom-0 left-0 h-full w-full z-[-9] opacity-[.7] bg-black"
-          ></div>
-          <NuxtImg
-            quality="20"
-            loading="lazy"
-            class="absolute object-cover bottom-0 left-0 h-full w-full z-[-99]"
-            src="/70001.jpg"
-            alt="#"
-          />
-        </div>
-      </div>
-      <div class="h-max w-screen flex justify-evenly gap-[1vw] px-[2vw]">
-        <div
-          class="h-[24vh] z-[1] w-[50%] overflow-hidden relative flex flex-col gap-[.8vh] items-start justify-between px-[1vw] pt-[1.8vh] pb-[1.2vh]"
-        >
-          <div
-            class="h-max w-full absolute top-[50%] left-0 text-center z-[9] bg-[#888] bg-opacity-[.4] backdrop-blur-[8px] text-[#fcfcfc] text-[1.8vh] py-[.8vh]"
-          >
-            COMING SOON!
-          </div>
-          <span
-            class="text-[3vh] w-[75%] font-[400] leading-[3.2vh] text-[#fff9]"
-            >Textiles</span
-          >
-          <div class="h-max w-full px-[2vw] flex justify-end">
-            <!-- <a
-              class="underline underline-offset-[.4vh] z-[99] text-[#e1e9e5] text-[1.8vh] font-[500] ml-[.2vw] text-[#e1e9e5]"
-              href="/accessories"
-              >view all</a
-            > -->
-          </div>
-          <div
-            class="absolute object-cover bottom-0 left-0 h-full w-full z-[-9] opacity-[.7] bg-black"
-          ></div>
-          <NuxtImg
-            quality="20"
-            loading="lazy"
-            class="absolute object-cover bottom-0 left-0 h-full w-full z-[-99]"
-            src="/80001.jpeg"
-            alt="#"
-          />
-        </div>
-        <div
-          class="h-[24vh] z-[1] w-[50%] overflow-hidden relative flex flex-col gap-[.8vh] items-start justify-between px-[1vw] pt-[1.8vh] pb-[1.2vh]"
-        >
-          <div
-            class="h-max w-full absolute top-[50%] left-0 text-center z-[9] bg-[#888] bg-opacity-[.4] backdrop-blur-[8px] text-[#fcfcfc] text-[1.8vh] py-[.8vh]"
-          >
-            COMING SOON!
-          </div>
-          <span
-            class="text-[3vh] font-[400] w-[75%] leading-[3.2vh] text-[#fff9]"
-            >Surface Courier
-          </span>
-          <div class="h-max w-full px-[2vw] flex justify-end">
-            <!-- <a
-              class="underline underline-offset-[.4vh] z-[99] text-[#e1e9e5] text-[1.8vh] font-[500] ml-[.2vw]"
-              href="#"
-              >view all</a
-            > -->
-          </div>
-          <div
-            class="absolute object-cover bottom-0 left-0 h-full w-full z-[-9] opacity-[.7] bg-black"
-          ></div>
-          <NuxtImg
-            quality="20"
-            loading="lazy"
-            class="absolute object-cover bottom-0 left-0 h-full w-full z-[-99]"
-            src="/10001.jpg"
+            src="/carpets/wall-to-wall.webp"
             alt="#"
           />
         </div>
