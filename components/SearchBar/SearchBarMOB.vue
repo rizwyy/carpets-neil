a3d32a50178270aa61dc2d9d717d9d3ba820caa0
Riswin Mohammed <123391105+rizwyy@users.noreply.github.com>
2024-07-08 07:57:36 +0530

    done

--- components/SearchBar/SearchBarMOB.vue
+++ components/SearchBar/SearchBarMOB.vue
@@ -3,7 +3,7 @@
     class="min-[990px]:hidden relative h-max w-screen flex flex-col items-center justify-center my-[1.8vh] px-[8vw] font-outfit"
   >
     <div
-      class="div_searchBar h-max w-full flex items-center bg-[#f7f5f2] px-[1vw] rounded-lg shadow-md"
+      class="div_searchBar h-max w-full flex items-center bg-[#f7f5f2] pl-[1vw] rounded-lg shadow-md overflow-hidden"
     >
       <input
         v-model="query"
@@ -12,21 +12,21 @@
         @blur="onBlur"
         type="text"
         placeholder="Find your dream product..."
-        class="input_searchBar h-max w-full bg-[#f7f5f2] text-[2vh] font-[400] flex-grow py-[1.8vh] rounded-sm pl-[4vw] outline-none focus:outline-none"
+        class="input_searchBar h-max w-full bg-[#f7f5f2] text-[2vh] font-[400] flex-grow pl-[4vw] outline-none focus:outline-none"
       />
       <div
         @click="onSearch"
-        class="flex h-full items-center relative justify-center px-[2.4vw] py-[1.2vh] rounded-full bg-[#172554]"
+        class="flex h-full items-center relative justify-center px-[2.6vw] py-[1.2vh] rounded bg-[#000]"
       >
         <div v-if="query === ''" class="h-full w-full">
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            width="20"
-            height="20"
+            width="24"
+            height="24"
             viewBox="0 0 24 24"
           >
             <path
-              fill="#f1f1f1"
+              fill="#fff"
               d="m19.6 21l-6.3-6.3q-.75.6-1.725.95T9.5 16q-2.725 0-4.612-1.888T3 9.5t1.888-4.612T9.5 3t4.613 1.888T16 9.5q0 1.1-.35 2.075T14.7 13.3l6.3 6.3zM9.5 14q1.875 0 3.188-1.312T14 9.5t-1.312-3.187T9.5 5T6.313 6.313T5 9.5t1.313 3.188T9.5 14"
             />
           </svg>
