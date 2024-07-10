10a25d19e6b7d602ccb6d826ed75ce6e9584837d
Riswin Mohammed <123391105+rizwyy@users.noreply.github.com>
2024-07-10 15:05:07 +0530

    done

--- components/DESKTOP/NavBarPC.vue
+++ components/DESKTOP/NavBarPC.vue
@@ -3,11 +3,7 @@
     class="max-[990px]:hidden h-max max-w-[100vw] overflow-x-hidden flex items-center justify-between py-[4vh] px-[6vw] bg-[#f1f1f1] font-outfit"
   >
     <div class="h-full w-[35%] flex items-center justify-start">
-      <button
-        @click="
-          giveAlert('Menu Page Under Development, Please come back later :(')
-        "
-      >
+      <button @click="openMenu">
         <svg
           xmlns="http://www.w3.org/2000/svg"
           width="28"
@@ -39,12 +35,74 @@
       >
     </div>
   </section>
+  <div
+    v-if="isMenuOpen"
+    class="menuPageMOB opacity-0 invisible font-outfit fixed top-0 left-0 h-screen w-screen bg-[#f5f3f1] z-[99]"
+  >
+    <div class="h-[14vh] w-screen px-[4vw] items-center flex justify-between">
+      <a
+        href="/"
+        class="h-max w-max font-[300] text-[5vh] text-[#333] tracking-[-.1vw]"
+      >
+        WOLTIZ
+      </a>
+      <span
+        ><svg
+          fill="#333"
+          @click="closeMenu"
+          height="32"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="m12 13.4l-4.9 4.9q-.275.275-.7.275t-.7-.275q-.275-.275-.275-.7t.275-.7l4.9-4.9l-4.9-4.9q-.275-.275-.275-.7t.275-.7q.275-.275.7-.275t.7.275l4.9 4.9l4.9-4.9q.275-.275.7-.275t.7.275q.275.275.275.7t-.275.7L13.4 12l4.9 4.9q.275.275.275.7t-.275.7q-.275.275-.7.275t-.7-.275z"
+          ></path></svg
+      ></span>
+    </div>
+    <div
+      class="h-[66vh] w-screen flex flex-col justify-center gap-[2vh] items-start px-[4vw] text-[3.8vh]"
+    >
+      <a href="/">Home</a><a href="/about">About</a><span>Contact</span
+      ><a href="/privacy">Privacy Policy</a>
+    </div>
+    <div class="h-[20vh] w-screen flex flex-col">
+      <div
+        class="flex h-full w-full justify-between px-[4vw] items-center"
+      ></div>
+      <div class="flex h-full w-full justify-between px-[4vw] items-center">
+        <span>Under Development | Demo</span>
+        <span>Woltiz</span>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup>
-function giveAlert(msg) {
-  alert(msg);
+import gsap from "gsap";
+const isMenuOpen = ref(false);
+function openMenu() {
+  isMenuOpen.value = true;
+  gsap.to(".menuPageMOB", {
+    autoAlpha: 1,
+    duration: 0.2,
+    ease: "power4.in",
+  });
+}
+function closeMenu() {
+  isMenuOpen.value = false;
+  gsap.to(".menuPageMOB", {
+    autoAlpha: 0,
+    duration: 0.2,
+    ease: "power4.out",
+  });
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style scoped>
+.font-outfit {
+  font-family: "Outfit", sans-serif;
+  font-optical-sizing: auto;
+  font-weight: 300;
+  font-style: normal;
+}
+</style>
