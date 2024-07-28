40f0c8a65f0fdb12b5efe98da3257a29bdc3b7c1
Riswin Mohammed <123391105+rizwyy@users.noreply.github.com>
2024-07-28 16:40:44 +0530

    sas

--- pages/flooring/carpets/product/[id].vue
+++ pages/flooring/carpets/product/[id].vue
@@ -17,13 +17,8 @@
         >
           <!-- MOBILE -->
           <img
-            loading="lazy"
             class="object-cover h-[32vh] w-[98%]"
-            :src="
-              color
-                ? `https://iqhciavbeulhroqoskbu.supabase.co/storage/v1/object/public/images_carpets/${color}`
-                : `/carpets/${getColorAfterHyphen(color)}.webp`
-            "
+            :src="`https://iqhciavbeulhroqoskbu.supabase.co/storage/v1/object/public/images_carpets/${productColor}`"
             :alt="getColorAfterHyphen(productColor)"
           />
         </div>
@@ -258,13 +253,8 @@ class="price-details_container h-max w-max flex flex-col gap-[.2vh] items-end px
         <!-- R1 -->
         <div class="h-full w-[40%] pl-[4vw] pt-[1vh] bg-[#fff] rounded-md">
           <img
-            loading="eager"
             class="object-contain h-[72vh] w-[100%]"
-            :src="
-              color
-                ? `https://iqhciavbeulhroqoskbu.supabase.co/storage/v1/object/public/images_carpets/${color}`
-                : `/carpets/${getColorAfterHyphen(color)}.webp`
-            "
+            :src="`https://iqhciavbeulhroqoskbu.supabase.co/storage/v1/object/public/images_carpets/${productColor}`"
             :alt="getColorAfterHyphen(productColor)"
           />
         </div>
