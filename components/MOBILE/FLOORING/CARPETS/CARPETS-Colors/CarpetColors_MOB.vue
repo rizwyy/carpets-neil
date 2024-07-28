9d3033d4a866a73364643363d6400bce42a13f8e
Riswin Mohammed <123391105+rizwyy@users.noreply.github.com>
2024-07-28 14:26:25 +0530

    done

--- components/MOBILE/FLOORING/CARPETS/CARPETS-Colors/CarpetColors_MOB.vue
+++ components/MOBILE/FLOORING/CARPETS/CARPETS-Colors/CarpetColors_MOB.vue
@@ -40,7 +40,21 @@
         v-show="isMultiColoredOpted"
         class="h-full w-full bg-black bg-opacity-[.7] backdrop-blur-[8px] rounded-md absolute top-0 left-0 z-[99] flex items-center justify-center flex-col gap-[4vh]"
       >
-        <span class="text-[#f1f1f1] text-[3.2vh]">Enter Your Color</span>
+        <div class="h-max w-full flex justify-center items-center gap-[2vw]">
+          <span class="text-[#f1f1f1] text-[3.2vh]">Enter Your Color</span>
+          <svg
+            @click="clearColorSelections"
+            xmlns="http://www.w3.org/2000/svg"
+            width="20"
+            height="20"
+            viewBox="0 0 24 24"
+          >
+            <path
+              fill="#f1f1f1"
+              d="m12 13.4l2.9 2.9q.275.275.7.275t.7-.275t.275-.7t-.275-.7L13.4 12l2.9-2.9q.275-.275.275-.7t-.275-.7t-.7-.275t-.7.275L12 10.6L9.1 7.7q-.275-.275-.7-.275t-.7.275t-.275.7t.275.7l2.9 2.9l-2.9 2.9q-.275.275-.275.7t.275.7t.7.275t.7-.275zm0 8.6q-2.075 0-3.9-.788t-3.175-2.137T2.788 15.9T2 12t.788-3.9t2.137-3.175T8.1 2.788T12 2t3.9.788t3.175 2.137T21.213 8.1T22 12t-.788 3.9t-2.137 3.175t-3.175 2.138T12 22m0-2q3.35 0 5.675-2.325T20 12t-2.325-5.675T12 4T6.325 6.325T4 12t2.325 5.675T12 20m0-8"
+            />
+          </svg>
+        </div>
         <input
           class="h-[6vh] w-[80%] px-[4vw] bg-[#fff2] border-[2px] rounded-md text-[#fff]"
           type="text"
