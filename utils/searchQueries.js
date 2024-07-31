bae55852c14383dbaa7e215e5f871e638c6238ee
Riswin Mohammed <123391105+rizwyy@users.noreply.github.com>
2024-07-31 13:22:51 +0530

    s

---
+++ utils/searchQueries.js
@@ -0,0 +1,128 @@
+// const searchQueries = {
+//   // Applications
+//   fitness: "/flooring/gym/details",
+//   "strength & conditioning": "/flooring/gym/details",
+//   "group training": "/flooring/gym/details",
+//   "martial arts": "/flooring/gym/details",
+
+//   // Fitness Activities
+//   cardio: "/flooring/gym/details",
+//   "machine training": "/flooring/gym/details",
+//   "free weights": "/flooring/gym/details",
+//   "functional training": "/flooring/gym/details",
+
+//   // Strength & Conditioning Activities
+//   weightlifting: "/flooring/gym/details",
+//   powerlifting: "/flooring/gym/details",
+
+//   // Group Training Activities
+//   dance: "/flooring/gym/details",
+//   aerobic: "/flooring/gym/details",
+//   zumba: "/flooring/gym/details",
+//   step: "/flooring/gym/details",
+//   trx: "/flooring/gym/details",
+//   pilates: "/flooring/gym/details",
+
+//   // Martial Arts Activities
+//   judo: "/flooring/gym/details",
+//   kickboxing: "/flooring/gym/details",
+//   karate: "/flooring/gym/details",
+//   boxing: "/flooring/gym/details",
+//   taekwondo: "/flooring/gym/details",
+//   aikido: "/flooring/gym/details",
+
+//   // Flooring Types
+//   rubber: "/flooring/gym/details",
+//   pvc: "/flooring/gym/details",
+//   wooden: "/flooring/gym/details",
+//   "artificial grass": "/flooring/gym/details",
+
+//   // Raised Floor - Choose by Area
+//   "data center": "/flooring/raised/details",
+//   commercial: "/flooring/raised/details",
+//   industrial: "/flooring/raised/details",
+//   healthcare: "/flooring/raised/details",
+//   activity: "/flooring/raised/details",
+
+//   // Raised Floor - Choose by Application
+//   "server rooms": "/flooring/raised/details",
+//   "telecom rooms": "/flooring/raised/details",
+//   offices: "/flooring/raised/details",
+//   retail: "/flooring/raised/details",
+//   hospitality: "/flooring/raised/details",
+//   "public area": "/flooring/raised/details",
+//   "industrial facilities": "/flooring/raised/details",
+//   "food processing": "/flooring/raised/details",
+//   "chemical lab": "/flooring/raised/details",
+//   "clean rooms": "/flooring/raised/details",
+//   "facility rooms": "/flooring/raised/details",
+//   schools: "/flooring/raised/details",
+//   kitchens: "/flooring/raised/details",
+//   basements: "/flooring/raised/details",
+//   swimming: "/flooring/raised/details",
+
+//   // Raised Floor - Choose by Panel
+//   bare: "/flooring/raised/details",
+//   hpl: "/flooring/raised/details",
+//   pvc: "/flooring/raised/details",
+//   perforated: "/flooring/raised/details",
+
+//   //Sports Types
+//   indoor: "/flooring/sports/details",
+//   outdoor: "/flooring/sports/details",
+
+//   //Sports Indoor Activities
+//   badminton: "/flooring/sports/details",
+//   basketball: "/flooring/sports/details",
+//   squash: "/flooring/sports/details",
+//   volleyball: "/flooring/sports/details",
+//   futsal: "/flooring/sports/details",
+//   handball: "/flooring/sports/details",
+//   "table tennis": "/flooring/sports/details",
+//   "multi-sport indoor": "/flooring/sports/details",
+//   "indoor martial arts": "/flooring/sports/details",
+
+//   //Sports Outdoor Activities
+//   tracks: "/flooring/sports/details",
+//   football: "/flooring/sports/details",
+//   tennis: "/flooring/sports/details",
+//   "outdoor basketball": "/flooring/sports/details",
+//   hockey: "/flooring/sports/details",
+//   pickleball: "/flooring/sports/details",
+//   padel: "/flooring/sports/details",
+//   "outdoor volleyball": "/flooring/sports/details",
+//   "multi-sport outdoor": "/flooring/sports/details",
+//   "outdoor others": "/flooring/sports/details",
+
+//   //Sports Flooring Types
+//   "artificial grass": "/flooring/sports/details",
+//   rubber: "/flooring/sports/details",
+//   "wooden sprung": "/flooring/sports/details",
+//   "polyurethane (pu)": "/flooring/sports/details",
+//   "pvc sports": "/flooring/sports/details",
+//   "acrylic sports": "/flooring/sports/details",
+
+//   // Carpet Subcategories
+//   "wall-to-wall carpets": "/flooring/carpets/details",
+//   "carpet tiles": "/flooring/carpets/details",
+//   "area rugs": "/flooring/carpets/details",
+//   runners: "/flooring/carpets/details",
+
+//   // Carpet Styles
+//   solid: "/flooring/carpets/details",
+//   textured: "/flooring/carpets/details",
+//   design: "/flooring/carpets/details",
+//   customized: "/flooring/carpets/details",
+
+//   // Yarn Selection
+//   wool: "/flooring/carpets/details",
+//   "wool blend": "/flooring/carpets/details",
+//   nylon: "/flooring/carpets/details",
+//   polypropylene: "/flooring/carpets/details",
+
+//   // Product Page
+//   "pop-up disclaimer": "/flooring/carpets/details",
+//   "made-to-order": "/flooring/carpets/details",
+//   "customized products": "/flooring/carpets/details",
+// };
+// export default searchQueries;
