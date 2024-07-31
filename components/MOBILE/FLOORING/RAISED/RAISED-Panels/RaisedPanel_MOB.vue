91d6d31c3e643a7649525ca9d9aceb0de91eea71
Riswin Mohammed <123391105+rizwyy@users.noreply.github.com>
2024-07-31 08:06:51 +0530

    done

--- components/MOBILE/FLOORING/RAISED/RAISED-Panels/RaisedPanel_MOB.vue
+++ components/MOBILE/FLOORING/RAISED/RAISED-Panels/RaisedPanel_MOB.vue
@@ -175,6 +175,9 @@ function clearSurfaceFinish() {
 }
 
 const toggleSelect = (surface) => {
+  if (surface === "Bare" && !userStore.preference.color.includes("Grey")) {
+    userStore.preference.color.push("Grey");
+  }
   if (selectedThickness.value === surface) {
     selectedThickness.value = "";
     userStore.preference.spec_3 = "";
