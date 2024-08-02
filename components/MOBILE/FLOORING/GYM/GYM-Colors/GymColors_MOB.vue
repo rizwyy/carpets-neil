8907546680cde23384239a3b5b5fe9e06a999ea9
Riswin Mohammed <123391105+rizwyy@users.noreply.github.com>
2024-08-02 16:58:51 +0530

    done

--- components/MOBILE/FLOORING/GYM/GYM-Colors/GymColors_MOB.vue
+++ components/MOBILE/FLOORING/GYM/GYM-Colors/GymColors_MOB.vue
@@ -288,12 +288,27 @@
           v-for="color in removeCustomColor()"
           :key="color"
           :style="{ backgroundColor: getColorHex(color) }"
-          class="text-center border border-black rounded-md py-[.8vh] px-[1vw]"
+          class="text-center border border-black rounded-md py-[.4vh] px-[.4vw] flex justify-evenly items-center overflow-hidden"
         >
           <span
-            class="inline bg-[#000] bg-opacity-[.2] px-[2vw] rounded-sm py-[.2vh] font-[00] backdrop-blur-[8px] text-white text-[1.6vh]"
+            class="inline bg-[#000] bg-opacity-[.2] px-[2vw] rounded-sm py-[.4vh] font-[500] backdrop-blur-[8px] text-white text-[1.6vh] text-center"
           >
             {{ color }}
+          </span>
+          <span
+            @click="toggleSelect(color)"
+            class="h-max w-max flex items-center bg-[#000] bg-opacity-[.2] px-[.8vw] py-[.4vh] rounded-full"
+          >
+            <svg
+              class="inline-block h-[2.4vh]"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+            >
+              <path
+                fill="#f1f3f5"
+                d="m12 13.4l2.9 2.9q.275.275.7.275t.7-.275t.275-.7t-.275-.7L13.4 12l2.9-2.9q.275-.275.275-.7t-.275-.7t-.7-.275t-.7.275L12 10.6L9.1 7.7q-.275-.275-.7-.275t-.7.275t-.275.7t.275.7l2.9 2.9l-2.9 2.9q-.275.275-.275.7t.275.7t.7.275t.7-.275zm0 8.6q-2.075 0-3.9-.788t-3.175-2.137T2.788 15.9T2 12t.788-3.9t2.137-3.175T8.1 2.788T12 2t3.9.788t3.175 2.137T21.213 8.1T22 12t-.788 3.9t-2.137 3.175t-3.175 2.138T12 22m0-2q3.35 0 5.675-2.325T20 12t-2.325-5.675T12 4T6.325 6.325T4 12t2.325 5.675T12 20m0-8"
+              />
+            </svg>
           </span>
         </span>
       </div>
