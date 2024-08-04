a8c2cb3b2a00d85ad02466f0703a735962f1b9b7
Riswin Mohammed <123391105+rizwyy@users.noreply.github.com>
2024-08-04 14:53:02 +0530

    update

--- components/MOBILE/FLOORING/WOODEN/Wooden-Designs/WoodDesigns_MOB.vue
+++ components/MOBILE/FLOORING/WOODEN/Wooden-Designs/WoodDesigns_MOB.vue
@@ -36,6 +36,77 @@
         </button>
       </div>
       <div
+        v-show="userStore.preference.spec_2 === 'Cork Floor'"
+        class="h-max w-full flex flex-col gap-[1vh]"
+      >
+        <div class="w-[90vw] h-max flex justify-between gap-[2vw] z-[9]">
+          <!-- MOBILE -->
+          <!-- Planks -->
+          <div
+            :class="[
+              'h-[14vh] wooden_Design_Details_Mob-CONTAINER opacity-0 translate-y-[20%] w-full rounded-md  border-[2px] border-[#f1f1f1] flex items-end justify-between text-[#f1f1f1] px-[1.6vw] py-[.8vh] text-[2.4vh] relative cursor-pointer',
+              { selected: userStore.preference.spec_3 === 'Planks' },
+            ]"
+            @click="toggleSelect('Planks')"
+          >
+            <div
+              v-show="userStore.preference.spec_3 === 'Planks'"
+              class="selectedDiv h-full w-full from-[20%] absolute bottom-0 left-0 bg-opacity-[.3] z-[1] flex items-start justify-end px-[1vw] py-[1vh]"
+            >
+              <!-- Selected indicator without text -->
+              <span
+                class="absolute top-1 right-1 bg-white text-gray-500 px-2 py-1 text-[1.8vh] rounded-md"
+                >Selected</span
+              >
+            </div>
+            <div
+              class="h-full w-full bg-gradient-to-b from-[#0000] to-black from-[60%] absolute bottom-0 left-0 bg-opacity-[.3] z-[-1]"
+            ></div>
+            <NuxtImg
+              quality="50"
+              placeholder
+              loading="lazy"
+              class="h-full w-full object-cover absolute left-0 bottom-0 z-[-9]"
+              src="/wooden/plank-wood.webp"
+              alt="#"
+            />
+            <span>Planks</span>
+          </div>
+          <!-- MOBILE -->
+          <!-- Tiles -->
+          <div
+            :class="[
+              'h-[14vh] wooden_Design_Details_Mob-CONTAINER opacity-0 translate-y-[20%] w-full rounded-md  border-[2px] border-[#f1f1f1] flex items-end justify-between text-[#f1f1f1] px-[1.6vw] py-[.8vh] text-[2.4vh] relative cursor-pointer',
+              { selected: userStore.preference.spec_3 === 'Tiles' },
+            ]"
+            @click="toggleSelect('Tiles')"
+          >
+            <div
+              v-show="userStore.preference.spec_3 === 'Tiles'"
+              class="selectedDiv h-full w-full from-[20%] absolute bottom-0 left-0 bg-opacity-[.3] z-[1] flex items-start justify-end px-[1vw] py-[1vh]"
+            >
+              <!-- Selected indicator without text -->
+              <span
+                class="absolute top-1 right-1 bg-white text-gray-500 px-2 py-1 text-[1.8vh] rounded-md"
+                >Selected</span
+              >
+            </div>
+            <div
+              class="h-full w-full bg-gradient-to-b from-[#0000] to-black from-[60%] absolute bottom-0 left-0 bg-opacity-[.3] z-[-1]"
+            ></div>
+            <NuxtImg
+              placeholder
+              loading="lazy"
+              class="h-full w-full object-cover absolute left-0 bottom-0 z-[-9]"
+              src="/wooden/wooden-herringbone.webp"
+              alt="#"
+            />
+            <span>Tiles</span>
+          </div>
+        </div>
+      </div>
+      <div
+        v-show="!(userStore.preference.spec_2 === 'Cork Floor')"
         class="relative w-full overflow-x-auto no-scrollbar select-none flex gap-[2vw]"
       >
         <!-- SLIDE 1 -->
