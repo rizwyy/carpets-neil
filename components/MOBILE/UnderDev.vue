a80a0addcad0932689f162e50abd4dd6ff60207b
Riswin Mohammed <123391105+rizwyy@users.noreply.github.com>
2024-08-04 11:34:18 +0530

    s

--- components/MOBILE/UnderDev.vue
+++ components/MOBILE/UnderDev.vue
@@ -3,12 +3,12 @@
     class="h-[88vh] w-screen overflow-hidden bg-gradient-to-b from-[#fff] via-[#f1f1f1] to-[#ececec] from-[1%] flex items-center justify-center"
   >
     <div
-      class="flex h-max w-max items-center flex flex-col justify-center gap-[2vh] px-[2vw] text-center"
+      class="flex h-max w-max items-center flex flex-col justify-center gap-[2vh]"
     >
-      <span class="text-[#222] text-[4.2vh] font-outfit">
+      <span class="text-[#222] text-[3.2vh] font-outfit w-[84%] text-left">
         This page is under development. Please check back soon.
       </span>
-      <NuxtImg class="h-[18vh]" src="/workInProgress.png" />
+      <NuxtImg class="h-[16vh]" src="/workInProgress.png" />
     </div>
   </div>
 </template>
