1a4b0172fa5677bcf275e35d16d86209bc3d5783
Riswin Mohammed <123391105+rizwyy@users.noreply.github.com>
2024-08-25 11:27:29 +0530

    update

--- components/MOBILE/FLOORING/DetailsReviewCards/ReviewCardMOB.vue
+++ components/MOBILE/FLOORING/DetailsReviewCards/ReviewCardMOB.vue
@@ -5,6 +5,7 @@
         userStore.preference.flooring === `${flooring}` &&
         userStore.preference.spec_1 !== '' &&
         userStore.preference.spec_2 !== '' &&
+        userStore.preference.spec_3 !== '' &&
         userStore.preference.color.length > 0 &&
         userStore.preference.budget !== '' &&
         userStore.preference.orderMethod !== '' &&
