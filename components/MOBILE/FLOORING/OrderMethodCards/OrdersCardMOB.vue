8d455d16a9eb4688489bf4f5e5bb34a02f8c1189
Riswin Mohammed <123391105+rizwyy@users.noreply.github.com>
2024-08-27 20:48:04 +0530

    update

--- components/MOBILE/FLOORING/OrderMethodCards/OrdersCardMOB.vue
+++ components/MOBILE/FLOORING/OrderMethodCards/OrdersCardMOB.vue
@@ -5,7 +5,6 @@
         userStore.preference.flooring === `${flooring}` &&
         userStore.preference.spec_1 !== '' &&
         userStore.preference.spec_2 !== '' &&
-        userStore.preference.spec_3 !== '' &&
         userStore.preference.color.length > 0 &&
         userStore.preference.budget !== ''
       "
@@ -27,7 +26,6 @@
               userStore.preference.flooring === `${flooring}` &&
               userStore.preference.spec_1 !== '' &&
               userStore.preference.spec_2 !== '' &&
-              userStore.preference.spec_3 !== '' &&
               userStore.preference.color.length > 0 &&
               userStore.preference.budget !== ''
             "
@@ -58,7 +56,6 @@
               userStore.preference.flooring === `${flooring}` &&
               userStore.preference.spec_1 !== '' &&
               userStore.preference.spec_2 !== '' &&
-              userStore.preference.spec_3 !== '' &&
               userStore.preference.color.length > 0 &&
               userStore.preference.budget !== ''
             "
--- components/MOBILE/FLOORING/SPORTS/SPORTS-Types/SportTypes_MOB.vue
+++ components/MOBILE/FLOORING/SPORTS/SPORTS-Types/SportTypes_MOB.vue
@@ -1,95 +1,97 @@
 <template>
   <!-- MOBILE -->
-  <div
-    class="h-max w-[96vw] text-center py-[4vh] flex flex-col gap-[1vh] font-outfit font-[400] bg-gradient-to-br from-detailsFrom to-detailsTo rounded-md shadow-lg bg-opacity-[.6] px-[3vw]"
-  >
+  <div class="min-[990px]:hidden h-max w-screen flex justify-center pb-[10vh]">
     <div
-      class="h-max w-full flex items-center justify-between text-[3.2vh] z-[9]"
+      class="h-max w-[96vw] text-center py-[4vh] flex flex-col gap-[1vh] font-outfit font-[400] bg-gradient-to-br from-detailsFrom to-detailsTo rounded-md shadow-lg bg-opacity-[.6] px-[3vw]"
     >
-      <span class="text-left text-balance"> Choose Your Type </span>
-      <button
-        @click="clearAllSelections"
-        v-show="spec_1.length"
-        class="h-max bg-white text-gray-500 px-[1vw] py-[.8vh] text-detailsContainer_clearAllText_MOB rounded-md flex gap-[1vw] items-center"
-      >
-        Clear All<svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="14"
-          height="14"
-          viewBox="0 0 24 24"
-        >
-          <path
-            fill="currentColor"
-            d="m12 13.4l2.9 2.9q.275.275.7.275t.7-.275t.275-.7t-.275-.7L13.4 12l2.9-2.9q.275-.275.275-.7t-.275-.7t-.7-.275t-.7.275L12 10.6L9.1 7.7q-.275-.275-.7-.275t-.7.275t-.275.7t.275.7l2.9 2.9l-2.9 2.9q-.275.275-.275.7t.275.7t.7.275t.7-.275zm0 8.6q-2.075 0-3.9-.788t-3.175-2.137T2.788 15.9T2 12t.788-3.9t2.137-3.175T8.1 2.788T12 2t3.9.788t3.175 2.137T21.213 8.1T22 12t-.788 3.9t-2.137 3.175t-3.175 2.138T12 22m0-2q3.35 0 5.675-2.325T20 12t-2.325-5.675T12 4T6.325 6.325T4 12t2.325 5.675T12 20m0-8"
-          />
-        </svg>
-      </button>
-    </div>
-    <div class="h-max w-full flex justify-between gap-[2vw] z-[9]">
-      <!-- INDOOR -->
       <div
-        :class="[
-          'h-[14vh] w-full rounded-md border-[2px] border-[#f1f1f1]  w flex items-end justify-between text-[#f1f1f1] px-[1.6vw] py-[.8vh] text-[2.4vh] relative cursor-pointer',
-          { selected: spec_1.includes('Indoor') },
-        ]"
-        @click="toggleSelect('Indoor')"
+        class="h-max w-full flex items-center justify-between text-[3.2vh] z-[9]"
       >
-        <div
-          v-show="spec_1.includes('Indoor')"
-          class="selectedDiv h-full w-full from-[20%] absolute bottom-0 left-0 bg-opacity-[.3] z-[1] flex items-start justify-end px-[1vw] py-[1vh]"
+        <span class="text-left text-balance"> Choose Your Type </span>
+        <button
+          @click="clearAllSelections"
+          v-show="spec_1.length"
+          class="h-max bg-white text-gray-500 px-[1vw] py-[.8vh] text-detailsContainer_clearAllText_MOB rounded-md flex gap-[1vw] items-center"
         >
-          <!-- Selected indicator without text -->
-          <span
-            class="absolute top-1 right-1 bg-white text-gray-500 px-2 py-1 text-[1.8vh] rounded-md"
-            >Selected</span
+          Clear All<svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="14"
+            height="14"
+            viewBox="0 0 24 24"
           >
-        </div>
-        <div
-          class="h-full w-full bg-gradient-to-b from-[#0000] to-black from-[60%] absolute bottom-0 left-0 bg-opacity-[.3] z-[-1]"
-        ></div>
-        <NuxtImg
-          preload
-          quality="50"
-          placeholder
-          loading="eager"
-          class="h-full w-full object-cover absolute left-0 bottom-0 z-[-9]"
-          src="/sports/indoor.webp"
-          alt=""
-        />
-        <span class="text-left text-balance">Indoor</span>
+            <path
+              fill="currentColor"
+              d="m12 13.4l2.9 2.9q.275.275.7.275t.7-.275t.275-.7t-.275-.7L13.4 12l2.9-2.9q.275-.275.275-.7t-.275-.7t-.7-.275t-.7.275L12 10.6L9.1 7.7q-.275-.275-.7-.275t-.7.275t-.275.7t.275.7l2.9 2.9l-2.9 2.9q-.275.275-.275.7t.275.7t.7.275t.7-.275zm0 8.6q-2.075 0-3.9-.788t-3.175-2.137T2.788 15.9T2 12t.788-3.9t2.137-3.175T8.1 2.788T12 2t3.9.788t3.175 2.137T21.213 8.1T22 12t-.788 3.9t-2.137 3.175t-3.175 2.138T12 22m0-2q3.35 0 5.675-2.325T20 12t-2.325-5.675T12 4T6.325 6.325T4 12t2.325 5.675T12 20m0-8"
+            />
+          </svg>
+        </button>
       </div>
-
-      <!-- OUTDOOR -->
-      <div
-        :class="[
-          'h-[14vh] w-full rounded-md border-[2px] border-[#f1f1f1]  w flex items-end justify-between text-[#f1f1f1] px-[1.6vw] py-[.8vh] text-[2.4vh] relative cursor-pointer',
-          { selected: spec_1.includes('Outdoor') },
-        ]"
-        @click="toggleSelect('Outdoor')"
-      >
+      <div class="h-max w-full flex justify-between gap-[2vw] z-[9]">
+        <!-- INDOOR -->
         <div
-          v-show="spec_1.includes('Outdoor')"
-          class="selectedDiv h-full w-full from-[20%] absolute bottom-0 left-0 bg-opacity-[.3] z-[1] flex items-start justify-end px-[1vw] py-[1vh]"
+          :class="[
+            'h-[14vh] w-full rounded-md border-[2px] border-[#f1f1f1]  w flex items-end justify-between text-[#f1f1f1] px-[1.6vw] py-[.8vh] text-[2.4vh] relative cursor-pointer',
+            { selected: spec_1.includes('Indoor') },
+          ]"
+          @click="toggleSelect('Indoor')"
         >
-          <!-- Selected indicator without text -->
-          <span
-            class="absolute top-1 right-1 bg-white text-gray-500 px-2 py-1 text-[1.8vh] rounded-md"
-            >Selected</span
+          <div
+            v-show="spec_1.includes('Indoor')"
+            class="selectedDiv h-full w-full from-[20%] absolute bottom-0 left-0 bg-opacity-[.3] z-[1] flex items-start justify-end px-[1vw] py-[1vh]"
           >
+            <!-- Selected indicator without text -->
+            <span
+              class="absolute top-1 right-1 bg-white text-gray-500 px-2 py-1 text-[1.8vh] rounded-md"
+              >Selected</span
+            >
+          </div>
+          <div
+            class="h-full w-full bg-gradient-to-b from-[#0000] to-black from-[60%] absolute bottom-0 left-0 bg-opacity-[.3] z-[-1]"
+          ></div>
+          <NuxtImg
+            preload
+            quality="50"
+            placeholder
+            loading="eager"
+            class="h-full w-full object-cover absolute left-0 bottom-0 z-[-9]"
+            src="/sports/indoor.webp"
+            alt=""
+          />
+          <span class="text-left text-balance">Indoor</span>
         </div>
+
+        <!-- OUTDOOR -->
         <div
-          class="h-full w-full bg-gradient-to-b from-[#0000] to-black from-[60%] absolute bottom-0 left-0 bg-opacity-[.3] z-[-1]"
-        ></div>
-        <NuxtImg
-          preload
-          quality="50"
-          placeholder
-          loading="eager"
-          class="h-full w-full object-cover absolute left-0 bottom-0 z-[-9]"
-          src="/sports/outdoor.webp"
-          alt=""
-        />
-        <span class="text-left text-balance">Outdoor</span>
+          :class="[
+            'h-[14vh] w-full rounded-md border-[2px] border-[#f1f1f1]  w flex items-end justify-between text-[#f1f1f1] px-[1.6vw] py-[.8vh] text-[2.4vh] relative cursor-pointer',
+            { selected: spec_1.includes('Outdoor') },
+          ]"
+          @click="toggleSelect('Outdoor')"
+        >
+          <div
+            v-show="spec_1.includes('Outdoor')"
+            class="selectedDiv h-full w-full from-[20%] absolute bottom-0 left-0 bg-opacity-[.3] z-[1] flex items-start justify-end px-[1vw] py-[1vh]"
+          >
+            <!-- Selected indicator without text -->
+            <span
+              class="absolute top-1 right-1 bg-white text-gray-500 px-2 py-1 text-[1.8vh] rounded-md"
+              >Selected</span
+            >
+          </div>
+          <div
+            class="h-full w-full bg-gradient-to-b from-[#0000] to-black from-[60%] absolute bottom-0 left-0 bg-opacity-[.3] z-[-1]"
+          ></div>
+          <NuxtImg
+            preload
+            quality="50"
+            placeholder
+            loading="eager"
+            class="h-full w-full object-cover absolute left-0 bottom-0 z-[-9]"
+            src="/sports/outdoor.webp"
+            alt=""
+          />
+          <span class="text-left text-balance">Outdoor</span>
+        </div>
       </div>
     </div>
   </div>
