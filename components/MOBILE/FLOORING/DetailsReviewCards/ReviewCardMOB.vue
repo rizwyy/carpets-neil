d7dd9c448747c6c88a56f7b4aae833735bef4854
Riswin Mohammed <123391105+rizwyy@users.noreply.github.com>
2024-08-30 06:08:46 +0530

    update

--- components/MOBILE/FLOORING/DetailsReviewCards/ReviewCardMOB.vue
+++ components/MOBILE/FLOORING/DetailsReviewCards/ReviewCardMOB.vue
@@ -1,6 +1,7 @@
 <template>
   <transition @beforeEnter="handleDetailsDOMEntry('revCard')">
-    <!-- v-show="
+    <div
+      v-show="
         userStore.preference.flooring === `${flooring}` &&
         userStore.preference.spec_1 !== '' &&
         userStore.preference.spec_2 !== '' &&
@@ -8,8 +9,7 @@
         userStore.preference.budget !== '' &&
         userStore.preference.orderMethod !== '' &&
         userStore.userData.name !== ''
-      " -->
-    <div
+      "
       class="min-[990px]:hidden h-max w-screen flex justify-center pt-[6vh] pb-[18vh]"
     >
       <div
