be11b3ec139ff4809ac3bd4e581fcb2e5328a203
Riswin Mohammed <123391105+rizwyy@users.noreply.github.com>
2024-09-01 13:56:37 +0530

    updated-orderMethods

---
+++ components/MOBILE/FLOORING/OrderMethodCards/OrderMethodButton.vue
@@ -0,0 +1,74 @@
+<template>
+  <button
+    @click="onClick"
+    :class="buttonClass"
+    :style="{
+      backgroundColor: containerBgColor,
+      borderColor: selected ? selectedBorderColor : '#ddd',
+      color: textColor,
+    }"
+    class="w-[44vw] h-[18vh] opacity-0 translate-y-[20%] rounded-3xl text-[2.4vh] font-[500] border-[2.8px] border-[#333] z-[999] text-white px-[2vw] flex flex-col items-start gap-[2vw] justify-between"
+  >
+    <div
+      :class="[
+        'h-max w-full px-[.4vw] flex',
+        selected ? 'justify-between' : 'justify-start',
+      ]"
+    >
+      <span :style="{ color: textColor }" class="pt-[1vh] text-start">
+        {{ label }}
+      </span>
+      <div v-show="selected" class="selectedDiv h-full w-full flex justify-end">
+        <span
+          :style="{
+            color: selectedBorderColor,
+          }"
+          class="h-max w-max font-[600]"
+          >O</span
+        >
+      </div>
+    </div>
+    <div
+      class="h-max w-max flex w-full flex justify-start pb-[1vh] pl-[1vw] text-[6.2vh]"
+    >
+      <WhatsappIcon v-if="label === 'WhatsApp'" />
+      <MailIcon v-else />
+    </div>
+  </button>
+</template>
+
+<script setup>
+import MailIcon from "~/public/icons/mailIcon.vue";
+import WhatsappIcon from "~/public/icons/whatsappIcon.vue";
+
+defineProps({
+  label: {
+    type: String,
+    required: true,
+  },
+  selected: {
+    type: Boolean,
+    default: false,
+  },
+  onClick: {
+    type: Function,
+    required: true,
+  },
+  buttonClass: {
+    type: String,
+    default: "",
+  },
+  containerBgColor: {
+    type: String,
+    default: "#111", // Default background color
+  },
+  selectedBorderColor: {
+    type: String,
+    default: "#00ff00", // Default border color when selected
+  },
+  textColor: {
+    type: String,
+    default: "#FFFFFF", // Default text color
+  },
+});
+</script>
--- components/MOBILE/FLOORING/OrderMethodCards/OrdersCardMOB.vue
+++ components/MOBILE/FLOORING/OrderMethodCards/OrdersCardMOB.vue
@@ -14,137 +14,71 @@
         class="h-max w-[96%] flex flex-col gap-[0vh] bg-gradient-to-br from-detailsFrom to-detailsTo pt-[4vh] pb-[4vh] rounded-md shadow-lg"
       >
         <span
-          class="w-[75%] px-[4vw] text-[3.2vh] leading-[3.8vh] OrderMethods_MOB-HEADING opacity-0 translate-x-[40%] text-detailsHeadCLR"
-          >Select Your Preferred Ordering Method:</span
+          class="w-[94%] px-[4vw] tracking-[-.1vw] font-[400] leading-[3.8vh] OrderMethods_MOB-HEADING opacity-0 translate-x-[40%] text-detailsHeadCLR text-detailsContainer_heading_MOB"
         >
+          How to Connect?
+        </span>
         <div
           class="h-max w-full flex items-center justify-center gap-[4vw] px-[2vw] mt-[4vh]"
         >
-          <button
+          <OrderMethodButton
+            label="WhatsApp"
+            :selected="userStore.preference.orderMethod === 'whatsapp'"
             @click="toggleOrderMethod('whatsapp')"
-            v-show="
-              userStore.preference.flooring === `${flooring}` &&
-              userStore.preference.spec_1 !== '' &&
-              userStore.preference.spec_2 !== '' &&
-              userStore.preference.color.length > 0 &&
-              userStore.preference.budget !== ''
-            "
-            class="w-[44vw] OrderMethods_MOB-CONTAINER opacity-0 translate-y-[20%] rounded-md text-[2.4vh] font-[500] bg-[#111] border-[2px] border-[#333] z-[999] text-white px-[2vw] flex items-center gap-[2vw] justify-between"
-          >
-            <!-- MOBILE -->
-            <span
-              class="bg-gradient-to-r flex-[85] py-[3.2vh] from-[#fff] via-[#ececec] shadow-2xl to-[#ececec] bg-clip-text text-transparent bg-300% text-center px-[4vw] animate-gradient_1 uppercase tracking-[.4vw]"
-            >
-              WhatsApp</span
-            >
-            <div
-              v-show="userStore.preference.orderMethod === 'whatsapp'"
-              class="selectedDiv h-full w-full from-[20%] absolute bottom-0 left-0 bg-opacity-[.3] z-[1] flex items-start justify-end px-[1vw] py-[1vh]"
-            >
-              <!-- Selected indicator without text -->
-              <span
-                class="absolute top-1 right-1 bg-white text-gray-500 px-2 py-1 text-[1.4vh] rounded-md"
-                >Selected</span
-              >
-            </div>
-
-            <!-- MOBILE -->
-          </button>
-          <button
+            buttonClass="OrderMethods_MOB-CONTAINER"
+            containerBgColor="#e8e8e8"
+            selectedBorderColor="#555"
+            textColor="#444"
+          />
+          <OrderMethodButton
+            label="Mail"
+            :selected="userStore.preference.orderMethod === 'email'"
             @click="toggleOrderMethod('email')"
-            v-show="
-              userStore.preference.flooring === `${flooring}` &&
-              userStore.preference.spec_1 !== '' &&
-              userStore.preference.spec_2 !== '' &&
-              userStore.preference.color.length > 0 &&
-              userStore.preference.budget !== ''
-            "
-            class="w-[44vw] OrderMethods_MOB-CONTAINER opacity-0 translate-y-[20%] rounded-md tracking-[.3vw] text-[2.4vh] font-[500] bg-[#111] border-[2px] border-[#333] z-[999] text-white px-[2vw] flex items-center gap-[2vw] justify-between"
-          >
-            <div
-              v-show="userStore.preference.orderMethod === 'email'"
-              class="selectedDiv h-full w-full from-[20%] absolute bottom-0 left-0 bg-opacity-[.3] z-[1] flex items-start justify-end px-[1vw] py-[1vh]"
-            >
-              <!-- Selected indicator without text -->
-              <span
-                class="absolute top-1 right-1 bg-white text-gray-500 px-2 py-1 text-[1.4vh] rounded-md"
-                >Selected</span
-              >
-            </div>
-            <!-- MOBILE -->
-            <span
-              class="bg-gradient-to-r flex-[85] py-[3.2vh] from-[#fff] via-[#ececec] shadow-2xl to-[#ececec] bg-clip-text text-transparent bg-300% text-center px-[4vw] animate-gradient_2 uppercase tracking-[.4vw]"
-            >
-              Mail</span
-            >
-
-            <!-- MOBILE -->
-          </button>
+            buttonClass="OrderMethods_MOB-CONTAINER"
+            containerBgColor="#e8e8e8"
+            selectedBorderColor="#555"
+            textColor="#444"
+          />
         </div>
       </div>
     </div>
   </transition>
-  <div
-    v-show="orderMethodWarning"
-    class="orderMethodWarningMOB h-screen w-screen bg-[#000] bg-opacity-[.2] backdrop-blur-[4px] fixed top-0 left-0 z-[9999]"
-  >
-    <div class="h-[100svh] w-full flex justify-center items-center">
-      <div
-        class="h-max w-[90vw] flex flex-col items-start gap-[4vh] bg-[#f1f1f1] pt-[2.8vh] rounded-lg"
-      >
-        <span class="text-detailsContainer_heading_MOB text-[#333] px-[3.2vw]"
-          >WARNING:</span
-        >
-        <span class="text-[2vh] px-[3.2vw] text-[#555]"
-          >Changing your order method will result in losing all previously saved
-          items associated with this contact number/email. <br />
-          <br />
-          Are you sure you want to proceed?</span
-        >
-        <div
-          class="h-max w-full flex justify-between gap-[4vw] border-t-[1.8px] border-[#ddd]"
-        >
-          <button
-            class="px-[8vw] text-[2vh] font-[500] text-[#222] py-[2vh] w-1/2 text-center border-r-[1.8px] border-[#ddd] tracking-[.3vw]"
-            @click="handleCloseOrderMethodWarningNo"
-          >
-            NO
-          </button>
-          <button
-            class="px-[8vw] text-[2vh] font-[500] text-[#222] py-[2vh] w-1/2 text-center border-[#ddd] tracking-[.3vw]"
-            @click="handleCloseOrderMethodWarningYes"
-          >
-            YES
-          </button>
-        </div>
-      </div>
-    </div>
-  </div>
+  <WarningModal
+    :show="orderMethodWarning"
+    :onYes="handleCloseOrderMethodWarningYes"
+    :onNo="handleCloseOrderMethodWarningNo"
+  />
 </template>
 
 <script setup>
 import useUserStore from "~/stores/user";
+import OrderMethodButton from "./OrderMethodButton.vue";
+import WarningModal from "./WarningModal.vue";
+
 const userStore = useUserStore();
-const restrictedAccess = useCookie("restrictedAccess");
 const userPreference = useCookie("userPreference");
 const { flooring } = defineProps(["flooring"]);
 const orderMethodWarning = ref(false);
 const cookieFound = ref(true);
+
 function setUserPreferenceCookie() {
   userPreference.value = userStore.preference;
   console.log("COOKIE SET::", toRaw(userPreference.value));
 }
+
 function handleCloseOrderMethodWarningYes() {
   userPreference.value.orderMethod = "";
   handleAutoAlpha("orderMethodWarningMOB", 0);
   ENABLE_SCROLL();
   orderMethodWarning.value = false;
 }
+
 function handleCloseOrderMethodWarningNo() {
   handleAutoAlpha("orderMethodWarningMOB", 0);
   ENABLE_SCROLL();
   orderMethodWarning.value = false;
 }
+
 function toggleOrderMethod(method) {
   if (cookieFound.value) {
     if (toRaw(userPreference.value.orderMethod) !== "") {
