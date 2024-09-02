92c44c11f4bfe0f4a6751ad5bd4063893fcf00e2
Riswin Mohammed <123391105+rizwyy@users.noreply.github.com>
2024-09-02 20:56:22 +0530

    done

--- pages/flooring/carpets/details.vue
+++ pages/flooring/carpets/details.vue
@@ -41,6 +41,16 @@ const LazyReviewCard = defineAsyncComponent(() =>
 const isMobile = ref(false);
 const existingUser = ref(false);
 
+// useHead({
+//   meta: [
+//     {
+//       name: "viewport",
+//       content:
+//         "width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no",
+//     },
+//   ],
+// });
+
 onMounted(() => {
   const userPreferenceCookie = useCookie("userPreference").value;
   if (
