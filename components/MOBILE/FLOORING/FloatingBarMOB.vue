5225204a7227112184be05669cf167e7a8a9557d
Riswin Mohammed <123391105+rizwyy@users.noreply.github.com>
2024-09-02 09:10:35 +0530

    update

--- components/MOBILE/FLOORING/FloatingBarMOB.vue
+++ components/MOBILE/FLOORING/FloatingBarMOB.vue
@@ -4,20 +4,19 @@ import useUserStore from "~/stores/user";
 
 const userStore = useUserStore();
 </script>
-
 <template>
   <div
     v-show="
-      userStore.preference.spec_1 !== '' && userStore.userData.name === ''
+      userStore.preference.spec_1 !== '' && userStore.preference.budget === ''
     "
-    class="h-[8.2vh] floatingCTA w-[100vw] z-[99999] fixed bottom-[0vh] left-[0vw] py-[1vh] backdrop-blur-[12px] bg-[#000] bg-opacity-[.9] left-[50%] translate-x-[-50%] flex font-outfit"
+    class="fixed bottom-0 left-1/2 transform -translate-x-1/2 w-full h-[8.2vh] z-[99999] backdrop-blur-lg bg-black bg-opacity-90 flex font-outfit"
   >
     <div
-      class="h-full w-[66%] border-r-[.1vw] flex items-center justify-center border-[#ececec9]"
+      class="flex items-center justify-center w-2/3 border-r border-gray-300"
     >
       <a
         href="/flooring/customize"
-        class="font-[500] text-[2.2vh] tracking-[.1vw] flex items-center gap-[1vw] justify-center bg-gradient-to-r from-[#fff9] via-[#E0FFFF] to-[#fff9] bg-clip-text text-transparent animate-gradient"
+        class="font-medium text-[2.2vh] flex items-center justify-center gap-[1vw] bg-gradient-to-r from-white/70 via-cyan-100 to-white/70 bg-clip-text text-transparent animate-gradient"
       >
         CUSTOM <span class="">{{ `${flooring}` }}</span>
         <svg
@@ -28,13 +27,15 @@ const userStore = useUserStore();
           <path
             fill="#e8e8e8"
             d="M2 6c0-1.505.78-3.08 2-4c0 .845.69 2 2 2a3 3 0 0 1 3 3c0 .386-.079.752-.212 1.091a74.515 74.515 0 0 1 2.191 1.808l-2.08 2.08a75.852 75.852 0 0 1-1.808-2.191A2.977 2.977 0 0 1 6 10c-2.21 0-4-1.79-4-4m12.152 6.848l1.341-1.341A4.446 4.446 0 0 0 17.5 12A4.5 4.5 0 0 0 22 7.5c0-.725-.188-1.401-.493-2.007L18 9l-2-2l3.507-3.507A4.446 4.446 0 0 0 17.5 3A4.5 4.5 0 0 0 13 7.5c0 .725.188 1.401.493 2.007L3 20l2 2l6.848-6.848a68.562 68.562 0 0 0 5.977 5.449l1.425 1.149l1.5-1.5l-1.149-1.425a68.562 68.562 0 0 0-5.449-5.977"
-          /></svg
-      ></a>
+          />
+        </svg>
+      </a>
     </div>
-    <div class="h-full w-[34%] flex items-center justify-center relative">
+    <div class="flex items-center justify-center w-1/3">
       <a
-        class="text-[#E8E8E8] font-[500] text-[2vh] flex gap-[1vw] items-center justify-center"
-        ><span>CHAT </span>
+        class="text-gray-300 font-medium text-[2vh] flex gap-[1vw] items-center justify-center"
+      >
+        <span>CHAT </span>
         <svg
           xmlns="http://www.w3.org/2000/svg"
           class="h-[3vh]"
@@ -47,25 +48,14 @@ const userStore = useUserStore();
           <path
             fill="#E8E8E8"
             d="M22.26 14.57c0-2.84-2.87-5.14-6.41-5.14s-6.41 2.3-6.41 5.14s2.87 5.14 6.41 5.14c.58 0 1.14-.08 1.67-.2L20.98 21l-1.2-2.4c1.5-.94 2.48-2.38 2.48-4.03m-8.34-.32a.96.96 0 1 1 .96-.96c.01.53-.43.96-.96.96m3.85 0a.96.96 0 1 1 0-1.92a.96.96 0 0 1 0 1.92"
-          /></svg
-      ></a>
+          />
+        </svg>
+      </a>
     </div>
   </div>
 </template>
 
 <style scoped>
-.font-outfit {
-  font-family: "Outfit", sans-serif;
-  font-optical-sizing: auto;
-  font-style: normal;
-}
-
-.animate-gradient {
-  background-size: 300%;
-  -webkit-animation: animatedgradient 3.2s ease-in-out infinite;
-  -moz-animation: animatedgradient 3.2s ease-in-out infinite;
-  animation: animatedgradient 3.2s ease-in-out infinite normal;
-}
 @keyframes animatedgradient {
   0% {
     background-position: 0% 50%;
