8cd416fe3f7a4d5b00a253dd1a886fac2a92e1a5
Riswin Mohammed <123391105+rizwyy@users.noreply.github.com>
2024-09-14 07:08:46 +0530

    update

--- components/MOBILE/FLOORING/DesignerApnaBannerMOB.vue
+++ components/MOBILE/FLOORING/DesignerApnaBannerMOB.vue
@@ -2,16 +2,22 @@
   <section
     class="w-[90vw] mx-auto mt-[2vh] relative overflow-hidden rounded-lg shadow-md px-[5vw] pt-[3.4vh] pb-[2.8vh] text-center font-outfit flex items-center flex-col gap-[5.8vh]"
   >
+    <div
+      class="apnaBannerItem_Overlay backdrop-blur-[8px] absolute top-0 left-0 h-full w-full transition-all duration-[.4s] ease-in-out bg-gradient-to-b hover:to-[70%] to-transparent z-[-1]"
+    ></div>
     <NuxtImg
       src="/bannerImage.png"
       decoding="async"
       alt="#"
+      @load="handleImageLoad(0, 'apnaBannerIMG_PC', 'apnaBannerItem')"
       loading="lazy"
       class="apnaBannerIMG h-full w-full z-[-9] absolute top-0 left-0 object-cover"
     />
 
     <!-- First line -->
-    <div class="text-[#555] text-[4vh] tracking-[-.3vw]">
+    <div
+      class="apnaBannerItem_Text opacity-0 text-[#555] text-[4vh] tracking-[-.3vw]"
+    >
       WOLTIZ
       <span class="text-[#222] font-[400] tracking-[0vw] text-[3.6vh]"
         >For Designers</span
@@ -19,13 +25,17 @@
     </div>
 
     <!-- Second line -->
-    <h2 class="text-[#333] text-[2.8vh] font-[400] pb-[2vh]">
+    <h2
+      class="apnaBannerItem_Text opacity-0 text-[#333] text-[2.8vh] font-[400] pb-[2vh]"
+    >
       Unlock Designer Discounts!
     </h2>
 
     <!-- Fifth line -->
     <div class="h-max w-full flex justify-end">
-      <p class="w-[72%] text-[#888] text-[2vh] font-[400]">
+      <p
+        class="apnaBannerItem_Text opacity-0 w-[72%] text-[#888] text-[2vh] font-[400]"
+      >
         Are you a Designer? Unlock Exclusive Discounts Here!
       </p>
     </div>
@@ -36,7 +46,7 @@
     >
       <a
         href="/contact"
-        class="border-[2px] border-[#888] text-[#444] active:shadow-xl w-[45%] py-[1.8vh] shadow-md rounded-full transition-colors duration-300 text-[2.4vh] font-[500]"
+        class="apnaBannerItem_Text opacity-0 border-[2px] border-[#888] text-[#444] active:shadow-xl w-[45%] py-[1.8vh] shadow-md rounded-full transition-colors duration-300 text-[2.4vh] font-[500]"
       >
         Sign up
       </a>
