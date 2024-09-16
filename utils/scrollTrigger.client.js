2db40c0f9f65ea99a515724a667f0c5aeb3f181a
Riswin Mohammed <123391105+rizwyy@users.noreply.github.com>
2024-09-16 08:25:59 +0530

    upda

--- utils/scrollTrigger.client.js
+++ utils/scrollTrigger.client.js
@@ -1,16 +1,24 @@
 import gsap from "gsap";
-import { ScrollTrigger } from "gsap/all";
-gsap.registerPlugin(ScrollTrigger);
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 export default function handleScrollTrigger(el) {
-  gsap.to(`.${el}`, {
-    opacity: 1,
-    duration: 0.4,
-    y: 0,
-    scrollTrigger: {
-      trigger: `.${el}`,
-      start: "top bottom-=10vh", // Start when the top of the element is 10vh above the bottom of the viewport
-      toggleActions: "play none none none", // Plays the animation when entering the viewport
-    },
-  });
+  // Ensure this runs only on the client-side
+  if (process.client) {
+    // Register the ScrollTrigger plugin with GSAP
+    if (!gsap.core.globals().ScrollTrigger) {
+      gsap.registerPlugin(ScrollTrigger);
+    }
+
+    // Use GSAP to animate the element
+    gsap.to(`.${el}`, {
+      opacity: 1,
+      duration: 0.4,
+      y: 0,
+      scrollTrigger: {
+        trigger: `.${el}`,
+        start: "top bottom-=10vh", // Trigger when the element enters the viewport
+        toggleActions: "play none none none", // Only play the animation once when entering
+      },
+    });
+  }
 }
